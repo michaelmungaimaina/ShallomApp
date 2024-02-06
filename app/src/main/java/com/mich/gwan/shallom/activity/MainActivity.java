4f4a7e89deb343960f0e632b9ee29f8ab304fafc
MAINA MICHAEL <pantalemonmongah@gmail.com>
2024-02-07 00:31:58 +0300

    More and More adjustments

--- app/src/main/java/com/mich/gwan/shallom/activity/MainActivity.java
+++ app/src/main/java/com/mich/gwan/shallom/activity/MainActivity.java
@@ -9,13 +9,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.Window;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.navigation.NavigationView;
 import com.mich.gwan.shallom.R;
 import com.mich.gwan.shallom.databinding.ActivityMainLayoutBinding;
@@ -25,9 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainLayoutBinding binding;
 
-
-
     private Toolbar toolbar;
+
+    private TextView textViewName;
+    private TextView textViewGreeting;
+
+    private CardView cardViewSearch;
+    private CardView cardViewSettings;
+    private CardView cardViewNotification;
+
+    private ShapeableImageView profileImage;
 
     FrameLayout frameLayout;
     private BottomNavigationView bottomNavigationView;
@@ -38,12 +48,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainLayoutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        textViewName = binding.myToolbar.textViewNameDisplay;
+        textViewGreeting = binding.myToolbar.textViewGreetingText;
+        profileImage = binding.myToolbar.shapeableImageViewUserAvatar;
+        cardViewSearch = binding.myToolbar.cardViewSearch;
+        cardViewSettings = binding.myToolbar.cardViewSettings;
+        cardViewNotification = binding.myToolbar.cardViewNotification;
+        toolbar = binding.myToolbar.toolbar;
+
         // since, NoActionBar was defined in theme, we set toolbar as our action bar.
-        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         //this basically defines on click on each menu item.
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView = binding.bottomNavigation;
         Menu menu = bottomNavigationView.getMenu();
 
         //Inflate the first fragment,this is like Home fragment before user selects anything.
